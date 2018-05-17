d449364f20aff2f1383a8a627f0e25a98941dc05
Phan Dang <phannhatdang@phans-macbook-air.local>
2018-05-17 08:48:44 +0700

    BasicTextField

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController,UIDropInteractionDelegate,UIScrollViewDelegate
+class EmojiArtViewController: UIViewController,UIDropInteractionDelegate,UIScrollViewDelegate,
+    UICollectionViewDataSource , UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
 {
+
+    
     var emojiArtView = EmojiArtView()
     @IBOutlet weak var dropZone: UIView! {
         didSet {
@@ -23,6 +26,13 @@ class EmojiArtViewController: UIViewController,UIDropInteractionDelegate,UIScrol
             scrollView.delegate = self 
             scrollView.addSubview(emojiArtView)
         }
+    }
+    @IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        scrollViewWidth.constant = scrollView.contentSize.width
+        scrollViewHeight.constant = scrollView.contentSize.height
     }
     
     func viewForZooming(in scrollView: UIScrollView)->UIView? {
@@ -39,11 +49,46 @@ class EmojiArtViewController: UIViewController,UIDropInteractionDelegate,UIScrol
             let size = newValue?.size ?? CGSize.zero
             emojiArtView.frame = CGRect(origin: CGPoint.zero, size : size)
             scrollView?.contentSize = size
+            scrollViewWidth?.constant = size.width
+            scrollViewHeight?.constant = size.height
+
             if let dropZone = self.dropZone, size.width > 0, size.height > 0 {
                 scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width, dropZone.bounds.size.height/size.height)
             }
         }
     }
+    
+    var emojis = "😇😍🦊🐺🌽🍞🥌🛷🚖🚋📺🎚🖤✒️🔐💔🎵🎶".map {String($0)}
+    
+    
+    @IBOutlet weak var emojiCollectionView: UICollectionView! {
+        didSet {
+            emojiCollectionView.dataSource = self
+            emojiCollectionView.delegate = self
+        }
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    private var font: UIFont {
+        return UIFontMetrics(forTextStyle: .body).scaledFont(for: UIFont.preferredFont(forTextStyle: .body).withSize(64.0))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EmojiCell", for: indexPath)
+        if let emojiCell = cell as? EmojiCollectionViewCell {
+            let text = NSAttributedString(string: emojis[indexPath.item], attributes: [.font:font])
+            emojiCell.label.attributedText = text
+        }
+        return cell
+    }
+    
+    
+    
     
     
     
---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Phan Nhat Dang on 5/17/18.
+//  Copyright © 2018 Phan Nhat Dang. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
